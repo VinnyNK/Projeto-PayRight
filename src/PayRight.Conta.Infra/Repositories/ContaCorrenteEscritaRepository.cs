34b02649ee0ca2ad16d2763b667b0e6894b9749f
Vinicius Kerschner <kerschner.vinicius@gmail.com>
2022-01-10 19:44:02 -0300

    Adicionado configuracao de database

---
+++ src/PayRight.Conta.Infra/Repositories/ContaCorrenteEscritaRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PayRight.Conta.Domain.Entities;
+using PayRight.Conta.Domain.Repositories;
+using PayRight.Conta.Infra.Contexts;
+
+namespace PayRight.Conta.Infra.Repositories;
+
+public class ContaCorrenteEscritaRepository : Repository<ContaCorrente>, IContaCorrenteEscritaRepository
+{
+    public ContaCorrenteEscritaRepository(ContextoDb db) : base(db)
+    {
+    }
+
+    public async Task CriarContaCorrente(ContaCorrente contaCorrente)
+    {
+        await DbSet.AddAsync(contaCorrente);
+    }
+
+    public async Task<bool> Commit()
+    {
+        return await Db.SaveChangesAsync() != 0;
+    }
+}
