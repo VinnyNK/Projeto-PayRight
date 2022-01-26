6b035ca4085c899864c15aa0bf43d0e9e0d9f49a
Vinicius Kerschner <kerschner.vinicius@gmail.com>
2022-01-26 10:32:49 -0300

    criado servico grpc para validar conta corrente e usuarioid

---
+++ src/PayRight.Conta.Grpc/Repositories/ContaCorrenteRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+using Dapper;
+using MySqlConnector;
+
+namespace PayRight.Conta.Grpc.Repositories;
+
+public class ContaCorrenteRepository : IContaCorrenteRepository
+{
+    private IDbConnection _dbConnection;
+
+    public ContaCorrenteRepository(IConfiguration configuration)
+    {
+        _dbConnection = new MySqlConnection(configuration.GetConnectionString("DbConnection"));
+    }
+
+    public async Task<bool> ExisteContaCorrente(Guid contaCorrenteId, Guid usuarioId)
+    {
+        const string query = "SELECT COUNT(C.\"ID\") FROM contas_correntes C WHERE C.\"ID\" = @ContaCorrenteId AND C.\"UsuarioId\" = @UsuarioId";
+
+        return await _dbConnection.QueryFirstOrDefaultAsync<bool>(query, new
+        {
+            ContaCorrenteId = contaCorrenteId,
+            UsuarioId = usuarioId
+        });
+    }
+}
---
+++ src/PayRight.Conta.Grpc/Services/ContaCorrenteService.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Core;
+using PayRight.Conta.Grpc.Protos;
+using PayRight.Conta.Grpc.Repositories;
+
+namespace PayRight.Conta.Grpc.Services;
+
+public class ContaCorrenteService : ContaCorrenteProtoService.ContaCorrenteProtoServiceBase
+{
+    private readonly IContaCorrenteRepository _contaCorrenteRepository;
+
+
+    public ContaCorrenteService(IContaCorrenteRepository contaCorrenteRepository)
+    {
+        _contaCorrenteRepository = contaCorrenteRepository;
+    }
+
+    public override async Task<ValidarContaCorrenteResponse> ValidarContaCorrente(ValidarContaCorrenteRequest request, ServerCallContext context)
+    {
+        return new ValidarContaCorrenteResponse()
+        {
+            EhValido = await _contaCorrenteRepository.ExisteContaCorrente(Guid.Parse(request.ContaCorrenteId), Guid.Parse(request.ContaCorrenteId))
+        };
+    }
+}
