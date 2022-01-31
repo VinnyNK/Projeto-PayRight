f685620d26817936cd705bedf3776b0492318976
Vinicius Kerschner <kerschner.vinicius@gmail.com>
2022-01-31 16:45:06 -0300

    Alterado método de repositórios do projeto do servidor grpc da conta

--- src/PayRight.Conta.Grpc/Configurations/InjecaoDeDependenciaConfiguration.cs
+++ src/PayRight.Conta.Grpc/Configurations/InjecaoDeDependenciaConfiguration.cs
@@ -1,4 +1,8 @@
-﻿using PayRight.Conta.Grpc.Repositories;
+﻿using MediatR;
+using PayRight.Conta.Domain.Queries;
+using PayRight.Conta.Domain.Repositories;
+using PayRight.Conta.Infra.Repositories;
+using PayRight.Shared.Mediator;
 
 namespace PayRight.Conta.Grpc.Configurations;
 
@@ -7,7 +11,15 @@ public static class InjecaoDeDependenciaConfiguration
     public static IServiceCollection ResolverDependencias(this IServiceCollection service)
     {
         // Repositories
-        service.AddScoped<IContaCorrenteRepository, ContaCorrenteRepository>();
+        service.AddScoped<IContaCorrenteEscritaRepository, ContaCorrenteEscritaRepository>();
+        service.AddScoped<IContaCorrenteLeituraRepository, ContaCorrenteLeituraRepository>();
+        
+        // Queries
+        service.AddScoped<IContaCorrenteQueries, ContaCorrenteQueries>();
+        
+        // Mediatr
+        service.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
+        service.AddScoped<IMediatorHandler, MediatorHandler>();
         
         return service;
     }
--- src/PayRight.Conta.Grpc/Repositories/IContaCorrenteRepository.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace PayRight.Conta.Grpc.Repositories;
-
-public interface IContaCorrenteRepository
-{
-    Task<bool> ExisteContaCorrente(Guid contaCorrenteId, Guid usuarioId);
-}
